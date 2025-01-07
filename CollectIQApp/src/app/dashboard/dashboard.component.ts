af6dc66c754c7bbda6027a40dc1a91a385b06f3a
Anthony Wright <anthony@mrwrite.dev>
2025-01-07 17:35:21 -0500

    Updated SideNav Menu Items

--- CollectIQApp/src/app/dashboard/dashboard.component.ts
+++ CollectIQApp/src/app/dashboard/dashboard.component.ts
@@ -8,7 +8,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon'; 
 import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { NgxChartsModule, Color, ScaleType } from '@swimlane/ngx-charts';
 import { CommonModule } from '@angular/common';
 
@@ -31,6 +32,7 @@ import { Router } from '@angular/router';
     MatIconModule,
     MatListModule,
     MatButtonModule,
+    MatMenuModule,
     NgxChartsModule,
   ],
   templateUrl: './dashboard.component.html',
